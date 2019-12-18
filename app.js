6ddf56d21d744a309d3eb44c1756ef3d8dbf3b92
Pakhomova Anastasia <55537369+PakhomovaN@users.noreply.github.com>
2019-12-18 13:54:10 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -14,34 +14,59 @@ function findPostIndexById(id) {
     return posts.findIndex(o => o.id === id);
 }
 
-server.get('/posts/:lastSeenId', (req, res) => {
-    const lastSeenId = Number(req.params.lastSeenId);
-
-    if (lastSeenId === 0) {
-        res.send(posts.slice(posts.length - 5));
-        return;
-    }
-
-    res.send(posts.slice(index, index + 5));
-});
-
-server.post('/posts', (req, res) => {
-    const body = req.body;
-    const id = body.id;
-    if (id === 0) {
-        posts = [...posts, {id: nextId++, content: body.content, likes: 0}];
-        res.send(posts);
-        return;
-    }
-    const index = findPostIndexById(id);
-    if (index === -1) {
-        res.status(404).send(errorNotFound);
-        return;
-    }
-
-    posts = posts.map(o => o.id !== id ? o : {...o, content: body.content});
+server.get('/posts/seenPosts', (req, res) => {
     res.send(posts);
 });
+
+server.get('/posts/lastPosts/:lastSeenId', (req, res) => {
+    const lastSeenId = Number(req.params.lastSeenId);
+    const index = postId(lastSeenId);
+
+    let seenPosts;
+
+    if (lastSeenId === 0) {
+        if (posts.length <= 5) {
+            seenPosts = posts;
+        } else {
+            seenPosts = posts.slice(posts.length - 5);
+        }          
+    } else if (lastSeenId > 0 && lastSeenId <= 5) {
+        seenPosts = posts.slice(0, index);
+
+    } else {
+        seenPosts = posts.slice(index - 5, index);
+    }
+
+    res.send(seenPosts);
+});
+
+
+server.get('/posts/lastPostsLoad/:postId', (req, res) => {
+    const postId = Number(req.params.postId)
+    if (postId === posts[0].id) {
+        res.send(true);
+        return;
+    }
+    res.send(false);
+})
+
+server.post('/posts', (req, res) => { 
+    const body = req.body; 
+    const id = body.id; 
+    if (id === 0) { 
+        posts = [...posts, {id: nextId++, content: body.content, likes: 0}]; 
+        res.send(posts); 
+        return; 
+    } 
+    const index = findPostIndexById(id); 
+    if (index === -1) { 
+        res.status(404).send(errorNotFound); 
+        return; 
+    } 
+ 
+    posts = posts.map(o => o.id !== id ? o : {...o, content: body.content}); 
+    res.send(posts); 
+}); 
 
 server.delete('/posts/:id', (req, res) => {
     const id = Number(req.params.id);
